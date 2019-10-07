8c8479774d7f21392391ea30f3eab34216030ffb
Erik <eghijs@gmail.com>
2019-10-07 15:47:59 -0300

    Update script_monitoramento-sernsor_esq-dir.ino

--- inBox4/script_monitoramento-sernsor_esq-dir.ino
+++ inBox4/script_monitoramento-sernsor_esq-dir.ino
@@ -1,20 +1,28 @@
-int IResq;
-int IRdir;
+/*****************************************\
+| Código Calibragem de Sensor IF TCRT5000 |
+| Autor: Erik P. Ghijs - eghijs@gmail.com |
+|    Sinta-se livre para usar ou editar   |
+\*****************************************/
+int Sesq = A0; // Porta do sensor esquerdo
+int Sdir = A1; // Porta do sensor direito
+
+int IResq = 0; // Pegar o valor 0 do sensor
+int IRdir = 0; // Pegar o valor 0 do sensor
 
 void setup() {
   Serial.begin(9600);
   Serial.println("---[ Monitoramento dos Sensores ]---");
-  Serial.println("( SensorIR A0 )  ( SensorIR A1 )");  
+  Serial.println("( SensorIR - Esq )  ( SensorIR - Dir )");  
   Serial.println(); 
 }
 
 void loop() {
-  IResq = analogRead(A0); //De 0 a 1023
-  IRdir = analogRead(A1); 
+  IResq = analogRead(Sesq); //De 0 a 1023
+  IRdir = analogRead(Sdir); 
   Serial.print("S.Esq.: ");
   Serial.print(IResq);
   Serial.print("/t");
   Serial.print("S.Dir.: ");
   Serial.println(IRdir);
-delay(1000);
+delay(250);
 }
