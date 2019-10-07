e91700f912dd113e7a861d34c3da8d081fd523b3
Erik <eghijs@gmail.com>
2019-10-07 12:30:13 -0300

    Update script_monitoramento-sernsor_esq-dir.ino

--- inBox4/script_monitoramento-sernsor_esq-dir.ino
+++ inBox4/script_monitoramento-sernsor_esq-dir.ino
@@ -8,7 +8,10 @@ void setup() {
 void loop() {
   IResq = analogRead(A0); //De 0 a 1023
   IRdir = analogRead(A1); 
-  Serial.println(IResq);
-  Serial.print('/t');
+  Serial.print("S.Esq.: ");
+  Serial.print(IResq);
+  Serial.print("/t");
+  Serial.print("S.Dir.: ");
   Serial.println(IRdir);
+delay(1000);
 }
